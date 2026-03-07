93dac2f11533f783e17d8991df13b07173afab4b
Casey Tran <ctran156@calpoly.edu>
2026-03-07 15:24:15 +0000

    Update cron job installation script

--- cron.sh
+++ cron.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-SCRIPT_DIR="/home/ubuntu/ytm"
+SCRIPT_DIR=~/ytm
 VENV_PY="$SCRIPT_DIR/.venv/bin/python"
 SCRIPT="$SCRIPT_DIR/main.py"
-LOG="$SCRIPT_DIR/cron.log"
 
-CRON_SCHEDULE="0 12 * * *"
-CRON_LINE="$CRON_SCHEDULE cd $SCRIPT_DIR && $VENV_PY $SCRIPT >> $LOG 2>&1"
-
-# Removes any old line that references script (to avoid duplicates)
-(crontab -l 2>/dev/null | grep -Fv "$SCRIPT"; echo "$CRON_LINE") | crontab -
-
-echo "Cron job installed:"
+CRON_TIME="0 12 * * *"
+CRON_JOB="cd $SCRIPT_DIR && $VENV_PY $SCRIPT" 
+CRON_LOG="$SCRIPT_DIR/cron.log"
+CRON_LINE="$CRON_TIME $CRON_JOB >> $CRON_LOG 2>&1"
 echo "$CRON_LINE"
+if ! crontab -l 2>/dev/null | grep -Fxq "$CRON_LINE"; then
+    (crontab -l 2>/dev/null; echo "$CRON_LINE") | crontab -
+	echo "Cron job installed:"
+	echo "$CRON_LINE"
+fi
 
