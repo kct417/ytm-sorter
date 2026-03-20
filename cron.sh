87350a802142f8e6c8b3df1acd8ff79db940a543
Casey Tran <ctran156@calpoly.edu>
2026-03-20 08:05:02 +0000

    Format cron script

--- cron.sh
+++ cron.sh
@@ -2,11 +2,13 @@ DIR=~/ytm-sorter
 VENV=~/ytm-sorter/.venv/bin/python
 SCRIPT=~/ytm-sorter/main.py
 
-JOB="cd $DIR && $VENV $SCRIPT" 
-LOG=~/.cron.log
+JOB="cd $DIR && $VENV $SCRIPT"
+LOG=~/.config/cron/cron.log
 CRON="0 12 * * * $JOB >> $LOG 2>&1"
 if ! crontab -l 2>/dev/null | grep -Fxq "$CRON"; then
-    (crontab -l 2>/dev/null; echo "$CRON") | crontab -
+	(
+		crontab -l 2>/dev/null
+		echo "$CRON"
+	) | crontab -
 	echo "Cron job installed: $CRON"
 fi
-
